8d66415efbf0aa25438eaafcb927e16e6c870c1e
LilAndy2 <iulian.andrei.manea@gmail.com>
2025-03-28 17:29:39 +0200

    added logging logic and basic implementation for the thread pool. added comments for clarity

--- app/__init__.py
+++ app/__init__.py
@@ -2,17 +2,43 @@ import os
 from flask import Flask
 from app.data_ingestor import DataIngestor
 from app.task_runner import ThreadPool
+import logging
+import logging.handlers
 
+# Create 'results' directory if it doesn't exist
 if not os.path.exists('results'):
     os.mkdir('results')
 
+# Initialize Flask app
 webserver = Flask(__name__)
-webserver.tasks_runner = ThreadPool()
 
 # webserver.task_runner.start()
 
+# Initialize DataIngestor with dataset file
 webserver.data_ingestor = DataIngestor("./nutrition_activity_obesity_usa_subset.csv")
 
+# Initialize job counter for tracking submitted jobs
 webserver.job_counter = 1
 
-from app import routes
+# Set up logging with RotatingFileHandler
+webserver.log = logging.getLogger(__name__)
+webserver.log.setLevel(logging.INFO)
+
+# Rotating log file: max size 300KB, keeps 5 backup files
+rotating_file_handler = logging.handlers.RotatingFileHandler(
+    'webserver.log', maxBytes=300000, backupCount=5
+)
+
+# Log format includes timestamp, module name, log level, and message
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S %Z'
+)
+rotating_file_handler.setFormatter(formatter)
+
+# Add handler to logger
+webserver.log.addHandler(rotating_file_handler)
+
+
+# Initialize thread pool for task execution
+webserver.tasks_runner = ThreadPool()
--- app/task_runner.py
+++ app/task_runner.py
@@ -1,19 +1,65 @@
 from queue import Queue
 from threading import Thread, Event
 import time
+import os
+from app.data_ingestor import DataIngestor
 
 class ThreadPool:
-    def __init__(self):
-        # You must implement a ThreadPool of TaskRunners
-        # Your ThreadPool should check if an environment variable TP_NUM_OF_THREADS is defined
-        # If the env var is defined, that is the number of threads to be used by the thread pool
-        # Otherwise, you are to use what the hardware concurrency allows
-        # You are free to write your implementation as you see fit, but
-        # You must NOT:
-        #   * create more threads than the hardware concurrency allows
-        #   * recreate threads for each task
-        # Note: the TP_NUM_OF_THREADS env var will be defined by the checker
-        pass
+    def __init__(self, logger=None):
+        '''
+            Function to initialize the Thread Pool with the number of threads
+            specified in the environmental variable TP_NUM_OF_THREADS.
+        '''
+        self.num_threads = int(os.getenv("TP_NUM_OF_THREADS", os.cpu_count()))
+
+        # Event to signal the Thread Pool to shutdown.
+        self.graceful_shutdown = Event()
+
+        # Queue of tasks that wait to be executed
+        self.tasks = Queue()
+
+        # List of thread workers
+        self.workers = []
+
+        # Dictionary to map the job_id to the task status
+        self.job_id_to_task = {}
+
+        self.data_ingestor = DataIngestor("./nutrition_activity_obesity_usa_subset.csv")
+        self.logger = logger
+
+        # Logging the thread pool's initialisation
+        self.logger.info(f"Thread pool has been initialised with {self.num_threads} threads.")
+        for i in range(self.num_threads):
+            self.workers.append(TaskRunner(self))
+            self.workers[i].start()
+            self.logger.info(f"Thread {i} started its job.")
+
+    def shutdown(self):
+        '''
+            Function to trigger the graceful_shutdown even and stop the Thread Pool
+        '''
+        if not self.graceful_shutdown.is_set():
+            # Sets the graceful_shutdown event and logs the thread pool's shutdown
+            self.graceful_shutdown.set()
+            self.logger.info("Thread Pool has been shut down.")
+
+            for thread in self.workers:
+                thread.join(timeout=10)
+                self.logger.info(f"Thread {thread.ident} joined.")
+    
+    def add_task(self, task):
+        '''
+            Function to add a task to the queue.
+        '''
+        # First check if the thread pool hasn't already been shut down
+        if not self.graceful_shutdown.is_set():
+            self.tasks.put(task)
+            self.logger.info(f"Task {task['job_id']} added to the queue.")
+        else:
+            self.logger.info("Thread Pool has been shut down, tasks can no longer be added.")
+        
+        return -1
+
 
 class TaskRunner(Thread):
     def __init__(self):
